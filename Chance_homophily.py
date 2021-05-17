3c1f4754ad2111ed6144cf5dfee92e9fc803bae5
Shyam Yadav <shyamyadav21099@gmail.com>
2021-05-17 23:31:38 +0530

    Add files via upload

---
+++ Chance_homophily.py
@@ -0,0 +1,16 @@
+from collections import Counter
+def chance_homophily(chars):
+    
+    chars_counts_dict = Counter(chars.values())
+    chars_counts = np.array(list(chars_counts_dict.values()))
+    chars_props  = chars_counts / sum(chars_counts)
+    return sum(chars_props**2)
+
+favorite_colors = {
+    "ankit":  "red",
+    "xiaoyu": "blue",
+    "mary":   "blue"
+}
+
+color_homophily = chance_homophily(favorite_colors)
+print(color_homophily)
